488f96f2a120c527329942ff3ae0826102bce3b3
Suvajit Chakrabarty <abhidec1990@gmail.com>
2018-09-24 17:05:46 -0400

    Adding seed data file

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,26 +1,12 @@
 const expect = require('expect');
 const request = require('supertest');
+const {ObjectID} = require('mongodb');
 
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
-const {ObjectID} = require('mongodb');
+const {todos, populateTodos} = require('./seed/seed');
 
-const todos = [{
-  _id: new ObjectID(),
-  text: 'First todo'
-},{
-  _id: new ObjectID(),
-  text: 'Second todo',
-  completed: true,
-  completedAt: 333
-}];
-
-
-beforeEach((done) => {
-  Todo.remove({}).then(() => {
-    return Todo.insertMany(todos);
-  }).then(() => done());
-});
+beforeEach(populateTodos);
 
 describe('POST /todos', () => {
   it('should create a new todo', (done)=>{
